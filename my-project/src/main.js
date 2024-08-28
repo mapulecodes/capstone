bc2d1d381ce9647c5a667af26dae9ef89b34e0b9
Mapule Mbuzwana <mbuzwanamapule@gmail.com>
2024-08-28 17:00:25 +0200

    changes

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import 'bootstrap/dist/css/bootstrap.min.css' //bootstrap
+import 'bootstrap' //bootstrap
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
---
+++ my-project/src/store/index.js
@@ -0,0 +1,5 @@
+// import { createStore } from 'vuex';
+// import axios from 'axios';
+// import { toast } from 'vue3-toastify';
+// import 'vue3-toastify/dist/index.css';
+
